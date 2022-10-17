1f64d9601dc7c3636a387885281f90307d37d87f
Vita Koval <mayevit@hotmail.com>
2022-10-17 16:41:38 +0300

    fix/padding

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -30,6 +30,7 @@ export function Contacts() {
         sx={{
           boxShadow:
             '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
+          padding: 2
         }}
       >
         <Typography
